ed42815569a54bb4b98c01156f52c83eb596db65
Ring0fire <andrew.al.gonzales@hotmail.com>
2020-04-14 20:50:06 -0700

    Add files via upload

---
+++ Assets/Scripts/TruffleGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TruffleGenerator : MonoBehaviour {
+
+	public ObjectPooler trufflePool;
+	
+	public float distanceBetweenTruffles;
+
+	public void SpawnTruffles (Vector3 startPosition)
+	{
+		GameObject truffle1 = trufflePool.GetPooledObject();
+		truffle1.transform.position = startPosition;
+		truffle1.SetActive (true);
+		
+		GameObject truffle2 = trufflePool.GetPooledObject();
+		truffle2.transform.position = new Vector3(startPosition.x - distanceBetweenTruffles, startPosition.y, startPosition.z);
+		truffle2.SetActive (true);
+		
+		GameObject truffle3 = trufflePool.GetPooledObject();
+		truffle3.transform.position = new Vector3(startPosition.x + distanceBetweenTruffles, startPosition.y, startPosition.z);
+		truffle3.SetActive (true);
+		
+	}
+	
+}
---
+++ Assets/Scripts/pickupPoints.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickupPoints : MonoBehaviour {
+	
+	public int scoreToGive;
+	
+	private ScoreManager theScoreManager;
+
+	private AudioSource truffGet;
+	//audio lines truffGet (truffle collection SFX)-18,33
+	//truffGet multiple Get resolution 33-37
+	// Use this for initialization
+	void Start () {
+		theScoreManager = FindObjectOfType<ScoreManager>();
+	
+		truffGet = GameObject.Find("TruffleCollect").GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.name == "Player")
+		{
+			theScoreManager.AddScore(scoreToGive);
+			gameObject.SetActive(false);
+			
+			if(truffGet.isPlaying)
+			{
+				truffGet.Stop();
+				truffGet.Play();
+			}
+			else{
+			truffGet.Play();
+			}
+		}
+	}
+	
+}
